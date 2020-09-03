e29620cf4a78dc69768034a35bdcce1e29aa063a
Abel Luck <abel@guardianproject.info>
2020-09-03 12:59:08 +0200

    fix: upgrade null label dependency

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@
 * that begin with a prefix. Also allos the instance to fetch its own tags.
 */
 module "label" {
-  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.16.0"
+  source      = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.19.2"
   namespace   = var.namespace
   name        = var.name
   stage       = var.stage
